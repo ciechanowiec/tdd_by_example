deb754eeacfd94fafba58241caebca947068d372
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-02-21 00:28:55 +0100

    Publish Chapter 4

--- src/main/java/com/Dollar.java
+++ src/main/java/com/Dollar.java
@@ -11,5 +11,19 @@ public class Dollar {
     public Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + amount;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Dollar comparedDollar = (Dollar) object;
+        return this.amount == comparedDollar.amount;        
+    }
     
 }
--- src/test/java/com/DollarTest.java
+++ src/test/java/com/DollarTest.java
@@ -2,6 +2,8 @@ package com;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DollarTest {
 
@@ -12,6 +14,13 @@ public class DollarTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        
     }
 
 }
