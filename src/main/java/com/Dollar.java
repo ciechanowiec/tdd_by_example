89f11f271c9918c385b69b62b962a392dec9778e
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-02-20 23:42:41 +0100

    Publish Chapter 1

---
+++ src/main/java/com/Dollar.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Dollar {
+
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        this.amount *= multiplier;
+    }
+    
+}
---
+++ src/test/java/com/MainTest.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testMiltiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+}
