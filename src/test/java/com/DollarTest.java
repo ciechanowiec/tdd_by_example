4747754243bd4dc4ea079c0a49a4e0642bcf347d
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-02-21 00:04:08 +0100

    Publish Chapter 2

--- src/test/java/com/DollarTest.java
+++ src/test/java/com/DollarTest.java
@@ -3,13 +3,15 @@ package com;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MainTest {
+public class DollarTest {
 
     @Test
     public void testMiltiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 
 }
