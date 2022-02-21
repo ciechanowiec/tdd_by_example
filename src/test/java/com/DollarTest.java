e75ff11fc741be3adea1677b7e6a4a268647e595
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-02-21 16:32:54 +0100

    Publish Chapter 4

--- src/test/java/com/DollarTest.java
+++ src/test/java/com/DollarTest.java
@@ -9,18 +9,15 @@ public class DollarTest {
 
     @Test
     public void testMiltiplication() {
-        Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        Dollar five = new Dollar(5);        
+        assertEquals(new Dollar(10), five.times(2));        
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
-        
     }
 
 }
