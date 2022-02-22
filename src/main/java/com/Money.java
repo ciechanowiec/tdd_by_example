48ac10c2d3badcab2a8e29f2fbcc026e8b33b182
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-02-22 17:19:51 +0100

    Publish Part I completed

--- src/main/java/com/Money.java
+++ src/main/java/com/Money.java
@@ -1,3 +1,60 @@
+/** 
+ * # TDD by Example. Part I
+ * 
+ * ## Basic Info
+ * This repository containts coding exercise from the book Test Driven Development. By Example (2003)
+ * written by Kent Beck. The repository embraces only Part I (The Money Example) of the mentioned book.
+ * 
+ * | Parameter                         | Data                                        |
+ * | :-------------------------------: | :-----------------------------------------: |
+ * | Date of creation:                 | February 2022                               |
+ * | Technologies used:                | Java SE, JUnit                              |
+ * | Time spent to create the program: | ~2 days                                     |
+ * | Author:                           | Herman Ciechanowiec, herman@ciechanowiec.eu |
+ * | License:                          | MIT No Attribution License                  |
+ * 
+ * ## To-Do List
+ * - [x] $5 + 10 CHF = $10 if rate is 2:1
+ * - [x] $5 + $5 = $10
+ * - [x] Return Money from $5 + $5
+ * - [x] Bank.reduce(Money)
+ * - [x] Reduce Money with conversion
+ * - [x] Reduce(Bank, String)
+ * - [x] $5 * 2 = $10
+ * - [x] Make “amount” private
+ * - [x] Dollar side effects?
+ * - [x] equals()
+ * - [x] hashCode()
+ * - [x] 5 CHF * 2 = 10 CHF		
+ * - [x] Dollar/Franc duplication
+ * - [x] Common equals
+ * - [x] Common times
+ * - [x] Compare Francs with Dollars
+ * - [x] Currency?
+ * - [x] Delete testFrancMultiplication?
+ * - [x] Sum.plus
+ * - [x] Expression.times
+ * 
+ * ## License
+ * The program is subject to MIT No Attribution License
+ * 
+ * Copyright © 2022 Herman Ciechanowiec
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ * 
+ * The Software is provided "as is", without warranty of any kind, express or implied,
+ * including but not limited to the warranties of merchantability, fitness for a
+ * particular purpose and noninfringement. In no event shall the authors or copyright
+ * holders be liable for any claim, damages or other liability, whether in an action
+ * of contract, tort or otherwise, arising from, out of or in connection with the
+ * Software or the use or other dealings in the Software.
+ * 
+ * */
+
 package com;
 
 public class Money implements Expression {
--- src/test/java/com/MoneyTest.java
+++ src/test/java/com/MoneyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
+    
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
